1e7af97e7d0c480faeba8e897142cf94dd30409c
yahui <tianya0yingzi@gmail.com>
2019-07-10 09:19:22 +0800

    fix: disable use django_rq when not using django_rq
    When I use celery, the bewlow code result execute smsish/tasks.py
    ```
    app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
    ```
    , and I used celery as a backend instead.

--- smsish/tasks.py
+++ smsish/tasks.py
@@ -1,17 +1,20 @@
-import django_rq
-from rq.decorators import job
+from django.conf import settings
 
-DEFAULT_QUEUE_NAME = "default"
-DEFAULT_REDIS_CONNECTION = django_rq.get_connection()
+if 'django_rq' in settings.INSTALLED_APPS:
+    import django_rq
+    from rq.decorators import job
 
-
-@job(DEFAULT_QUEUE_NAME, connection=DEFAULT_REDIS_CONNECTION)
-def send_sms(*args, **kwargs):
-	from smsish.sms import send_sms as _send_sms
-	return _send_sms(*args, **kwargs)
+    DEFAULT_QUEUE_NAME = "default"
+    DEFAULT_REDIS_CONNECTION = django_rq.get_connection()
 
 
-@job(DEFAULT_QUEUE_NAME, connection=DEFAULT_REDIS_CONNECTION)
-def send_mass_sms(*args, **kwargs):
-	from smsish.sms import send_mass_sms as _send_mass_sms
-	return _send_mass_sms(*args, **kwargs)
+    @job(DEFAULT_QUEUE_NAME, connection=DEFAULT_REDIS_CONNECTION)
+    def send_sms(*args, **kwargs):
+        from smsish.sms import send_sms as _send_sms
+        return _send_sms(*args, **kwargs)
+
+
+    @job(DEFAULT_QUEUE_NAME, connection=DEFAULT_REDIS_CONNECTION)
+    def send_mass_sms(*args, **kwargs):
+        from smsish.sms import send_mass_sms as _send_mass_sms
+        return _send_mass_sms(*args, **kwargs)
